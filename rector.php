56d8f0c806f0f570999339c3df1221b8bc1252e4
Tobias Köngeter <tobias.koengeter@bitandblack.com>
2024-07-23 13:32:05 +0200

    Update dependencies

--- rector.php
+++ rector.php
@@ -1,23 +1,24 @@
 <?php
 
 use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\MethodCall\MyCLabsMethodCallToEnumConstRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->parallel();
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withParallel()
+    ->withPaths([
         __DIR__,
-    ]);
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         __DIR__ . DIRECTORY_SEPARATOR . 'vendor',
+        MyCLabsMethodCallToEnumConstRector::class,
         PreferPHPUnitThisCallRector::class,
-    ]);
-    $rectorConfig->importNames();
-    $rectorConfig->import(LevelSetList::UP_TO_PHP_82);
-    $rectorConfig->sets([
+    ])
+    ->withSets([
         PHPUnitSetList::PHPUNIT_100,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-    ]);
-};
+    ])
+    ->withImportNames()
+    ->withPhpSets()
+;
